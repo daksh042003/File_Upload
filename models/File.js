438f2579dc4789b8d0c18ffbb6d8c5898bcb9096
Daksh Pandey <dakshpandey127@gmail.com>
2023-09-05 10:30:48 +0530

    changes

--- models/File.js
+++ models/File.js
@@ -1,4 +1,7 @@
 const mongoose =require("mongoose");
+const nodemailer = require("nodemailer");
+const {transporter} = require("../config/nodemailer");
+
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,6 +18,29 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 });
+
+
+fileSchema.post("save",async function(doc){
+    try{
+       
+         console.log("document -->",doc);
+
+         
+
+         let info =await transporter.sendMail({
+            from:`daksh`,
+            to:doc.email,
+            subject:"new file uploaded on cloudinary",
+            html:`<h2>hii </h2><p>file uploaded check :<a href = "${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+        })
+  
+         console.log("info-->",info);
+    }
+    catch(error){
+        console.error(error);
+    }
+})
+
 
 const File = mongoose.model("File",fileSchema);
 module.exports = File;
